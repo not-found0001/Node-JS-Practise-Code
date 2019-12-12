2bee61e4c9fe971fb580695a10e6de62049f528d
kaisar <mortujakaisar@gmail.com>
2019-12-12 16:41:09 +0600

    Store Data With JSON

--- notes.js
+++ notes.js
@@ -1,11 +1,35 @@
 
-const name = "MK"
+// const name = "MK"
 
-const getNotes = function(){
-    return "Your Notes..."
-}
+// const getNotes = function(){
+//     return "Your Notes..."
+// }
 
-module.exports = {
-    name,
-    getNotes
-}
+// module.exports = {
+//     name,
+//     getNotes
+// }
+
+const fs = require('fs')
+
+// const book = {
+//     name: 'Data Structure And Algorithm',
+//     description: 'very nice book'
+// }
+
+// const bookJSON = JSON.stringify(book) /// Convert Object To JSON
+// fs.writeFileSync('data.json', bookJSON);
+
+const readJSON = fs.readFileSync('data.json')
+const dataJSON = readJSON.toString()
+
+const obj = JSON.parse(dataJSON) /// Convert JSON To Object
+
+obj.name = 'Algorithm Design'
+const objJSON = JSON.stringify(obj)
+
+fs.writeFileSync('data.json', objJSON)
+
+const newJSON = fs.readFileSync('data.json', 'utf-8')
+const newObj = JSON.parse(newJSON)
+console.log(newObj.description)
